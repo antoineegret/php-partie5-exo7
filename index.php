5582ed0cc8f7b004f0d8189c6ed6cfe6113c642e
antoineegret <a.egret@laposte.net>
2019-12-10 16:01:50 +0100

    exo7 partie5 php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+//ceci et un tableau en php une genre de super variable
+//qui aurai des sous variable pour stocker plus d'information,
+$departement[80] = 'Somme';
+$departement[02] = 'Aisne';
+$departement[60] = 'Oise';
+$departement[62] = 'Pas de calais';
+$departement[59] = 'Nord';
+$departement[51] = 'Marne';
+//ici j'affiche avec echo la valeur de la 3eme case du tableau
+echo $departement[51];
+ ?>
